f0b4a4bc5f3b909fb835fb3d100727c4b0232c42
Nuno Dias <jookaada@protonmail.com>
2021-03-15 21:20:09 +0000

    Added feature to stress both stacks with any number of iterations

--- testStackEfficiency/src/testingEfficiency.java
+++ testStackEfficiency/src/testingEfficiency.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Program that tests the efficiency of two different implementations of the Stack ADT
  * @author Ren Amamiya
@@ -6,12 +7,16 @@
 public class testingEfficiency {
 	
 	public static void main(String[] args) {
+		int k;
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("Choose the amount of iterations to stress both stacks with: ");
+		k = sc.nextInt();
 		Stack<Integer> stack1 = new ArrayStack<Integer>();
 		Stack<Integer> stack2 = new LinkedListStack<Integer>();
 		double avg1 = 0, avg2 = 0;
 		
 		Stopwatch stp = new Stopwatch();
-		for(int i = 0; i < 5000; i++) {
+		for(int i = 0; i < k; i++) {
 			stp.reset();
 			for(int j = 0; j < 100; j++)
 				stack1.push(j);
@@ -21,7 +26,7 @@ public class testingEfficiency {
 		
 		avg1 /= 100;
 		
-		for(int i = 0; i < 5000; i++) {
+		for(int i = 0; i < k; i++) {
 			stp.reset();
 			for(int j = 0; j < 100; j++)
 				stack2.push(j);
@@ -35,5 +40,6 @@ public class testingEfficiency {
 			System.out.printf("The ArrayStack implementation is the more efficient one%nArrayStack Avg: %fs vs. LinkedListStack Avg: %fs%n", avg1, avg2);
 		else
 			System.out.printf("The LinkedListStack implementation is the more efficient one%nArrayStack Avg: %fs vs. LinkedListStack Avg: %fs%n", avg1, avg2);
+		System.out.printf("Tested with %d iterations%n", k);
 	}
 }
